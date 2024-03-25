4a6892aac896f602d707b112f53b3a0a30a9d3aa
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-25 02:46:07 -0700

    Add files via upload

---
+++ reductionscheme.py
@@ -0,0 +1,9 @@
+import collections
+import pathlib
+
+collections.ChainMap.__missing__.__globals__ 
+pathlib.Sequence.count.__reduce__.__subclasshook__
+\
+import _compression
+
+_compression.BaseStream.detach
